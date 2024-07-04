54e80adbc7fe28d38aeb857f0ca6e8c9a9ed4ad0
Louis <louis@jan.ai>
2024-07-04 18:07:16 +0700

    cortexso: uses node runtime

--- Formula/cortexso.rb
+++ Formula/cortexso.rb
@@ -1,35 +1,33 @@
+require "language/node"
 
 class Cortexso < Formula
   desc "Drop-in, local AI alternative to the OpenAI stack"
   homepage "https://jan.ai/cortex"
+  url "https://registry.npmjs.org/cortexso/-/cortexso-0.0.56.tgz"
+  sha256 "32a411ede26128a525003139ecf948aac85e9e530c5262826a581d8511da7d2f"
   license "Apache-2.0"
   head "https://github.com/janhq/cortex.git", branch: "dev"
-  version "0.4.17"
 
-  on_macos do
-    on_arm do
-      url "https://github.com/janhq/cortex/releases/download/v#{version}/cortex-#{version}-arm64-mac.tar.gz"
-      sha256 "131807cec90dc70aed3aa50670e00a55aca0148175798a6065e4bab3a7644e84"
-    end
-    on_intel do
-      url "https://github.com/janhq/cortex/releases/download/v#{version}/cortex-#{version}-amd64-mac.tar.gz"
-      sha256 "4e28ec51e0ddd72dd3fe66b8137dfc456110f19f1434861a5c7a745603073077"
-    end
-  end
-  on_linux do
-    url "https://github.com/janhq/cortex/releases/download/v#{version}/cortex-#{version}-amd64-linux.tar.gz"
-    sha256 "92917bba4d593f23e36c11045cd0db408eeaa3833afedb476500398866f2e53c"
-  end
+  depends_on "python-setuptools" => :build
+  depends_on "python@3.12" => :build
+  depends_on "node"
+
   def install
-    bin.install "cortex"
+    system "npm", "install", *Language::Node.std_npm_install_args(libexec)
+    # @cpu-instructions bundles all binaries for other platforms & architectures
+    # This deletes the non-matching architecture otherwise brew audit will complain.
+    prebuilds = libexec/"lib/node_modules/cortexso/node_modules/cpu-instructions/prebuilds"
+    (prebuilds/"darwin-x64").rmtree if OS.mac? && Hardware::CPU.arm?
+    (prebuilds/"darwin-arm").rmtree if OS.mac? && Hardware::CPU.intel?
+    bin.install_symlink Dir["#{libexec}/bin/*"]
   end
 
   test do
     port = free_port
     pid = fork { exec "#{bin}/cortex", "serve", "--port", port.to_s }
-    sleep 10
+    sleep 5
     begin
-      assert_match "OK", shell_output("curl -s localhost:#{port}/health")
+      assert_match "OK", shell_output("curl -s localhost:#{port}/v1/health")
     ensure
       Process.kill "SIGTERM", pid
     end
